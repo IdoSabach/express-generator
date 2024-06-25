202cec43715e529fde71ca7a6b4cc2a7c83f2fb3
Ido <Idosabach11@gmail.com>
2024-06-25 21:05:26 +0300

    Read All Date

--- routes/Contact.js
+++ routes/Contact.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Contact = require("../models/Contact");
 
+// create *C*rud
+///contact/post
 router.post("/post", async (req, res) => {
   try {
     const newContact = new Contact(req.body);
@@ -15,6 +17,24 @@ router.post("/post", async (req, res) => {
     } else {
       res.status(500).json({ msg: "Unable to create new Contact" });
     }
+  }
+});
+
+// Read c*R*ud ***Read All Date
+router.get("/post", async (req, res) => {
+  try {
+    Contact.find()
+      .then((contacts) => {
+        console.log(contacts);
+        res.status(200).json({ contacts: contacts });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ msg: "read error 1" });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "read error 2" });
   }
 });
 
